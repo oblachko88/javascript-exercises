c9bf75c7e02cb774d8fcaa7a6a5e5f20f7586e83
oblachko88 <88oblachko@gmail.com>
2024-08-26 22:49:08 +0300

    Commit Aug 26

--- exercise2.js
+++ exercise2.js
@@ -2,3 +2,5 @@ const sliceStringFromBehindUntilSignal = (str) => str.slice(str.lastIndexOf('.')
 
 console.log(sliceStringFromBehindUntilSignal('hello.txt'));
 console.log(sliceStringFromBehindUntilSignal('print.config.js'));
+
+const process = require();
---
+++ exercise5.js
@@ -0,0 +1,13 @@
+const stringNew = 'New!'
+
+const askString = prompt('Enter a new string: ')
+
+if (askString.startsWith('New!')) {
+  
+} else {
+  togetherString = stringNew + askString
+  return togetherString
+}
+
+console.log(togetherString)
+
