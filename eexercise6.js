5b798ccf9cf29464231113008630efd62668c623
oblachko88 <88oblachko@gmail.com>
2024-08-27 10:40:14 +0300

    Commit Aug 27

---
+++ eexercise6.js
@@ -0,0 +1,14 @@
+// const givenString = 'Hello world!'
+
+// const newString = (givenString) => {
+//   if (givenString.indexOf(0-2))
+// }
+
+const makeNewString = (str) => 
+  str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+
+console.log(makeNewString('abc'));
+console.log(makeNewString('abcdefgasds'));
+console.log(makeNewString('abc213213asd21as22'));
+
+console.log(makeNewString('ab'));
--- exercise5.js
+++ exercise5.js
@@ -1,13 +1,17 @@
-const stringNew = 'New!'
+// const stringNew = 'New!';
 
-const askString = prompt('Enter a new string: ')
+// const askString = prompt('Enter a new string: ');
 
-if (askString.startsWith('New!')) {
+// if (askString.startsWith('New!')) {
   
-} else {
-  togetherString = stringNew + askString
-  return togetherString
-}
+// } else {
+//   togetherString = stringNew + askString;
+//   return togetherString;
+// }
 
-console.log(togetherString)
+// console.log(togetherString);
 
+const addNew = (str) => 
+  str.indexOf('New!') === 0 ? str: `New! ${str}`
+
+console.log(addNew('New! Offers'));
